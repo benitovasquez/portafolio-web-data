92f33ef68676c13ec4e8b7aeed5e6f5d34a95d51
benitovasquez <ricardobenitovasquezroca@gmail.com>
2025-01-11 09:14:51 -0400

    7.1. revisando

--- myportfolio/settings/produccion.py
+++ myportfolio/settings/produccion.py
@@ -1,7 +1,12 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['.tudominio.com']  # Ajusta según tu dominio
+ALLOWED_HOSTS = ['portafolio-web-data.onrender.com']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 DATABASES = {
     'default': {
@@ -15,7 +20,38 @@ DATABASES = {
 }
 
 # Configuraciones de seguridad adicionales
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
+
+# Configurar la política de Referrer
+SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
+
+# Evitar el uso de contenido en marcos desde otros sitios
+X_FRAME_OPTIONS = 'DENY'
+
+# Agregar HSTS (HTTP Strict Transport Security)
+SECURE_HSTS_SECONDS = 31536000  # 1 año
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
